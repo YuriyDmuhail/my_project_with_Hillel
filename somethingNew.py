f859cee64792b98af01718c2b782275102bf79a0
Yurii <dmukhailo@gmail.com>
2025-03-13 20:46:56 +0200

    upd

--- somethingNew.py
+++ somethingNew.py
@@ -1,6 +1,18 @@
-some = (input("дай номер телефону:"))
+orderedNumber = int(input("Вибери значення більше 0:"))
 
-if len(some) == 9:
-    print("thank u")
-if  len(some) != 9:
-    print("fuck u")
+while True:
+    quessingNumber = int(input("Вгадай число тепер, урод:"))
+
+    if quessingNumber == orderedNumber:
+        print("Вгадала, сучка")
+        break
+    if quessingNumber > orderedNumber:
+        print("Дохуя, давай спочатку")
+    else:
+        print("Не, не в ту сторону вгадуєш")
+
+secPart = input("Напиши що хочеш :")
+if len(secPart) > 15:
+    print("Дохуя хочеш")
+else:
+    print("обво'язково зроблю це, муділа")
